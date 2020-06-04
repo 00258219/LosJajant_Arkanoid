697fd4d1c5e3ad468b4714d4c396a5a658d6627d
HeyChobe <00204119@uca.edu.sv>
2020-06-03 18:04:45 -0600

    Add: PlayerRegister userControl

---
+++ Source Code/Arkanoid/Vista/PlayerRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class PlayerRegister : UserControl
+    {
+        public PlayerRegister()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.Dock = DockStyle.Fill;
+            
+        }
+        
+        private void lblButton_MouseEnter(object sender, EventArgs e)
+        {
+            lblButton.BackColor = Color.FromArgb(125, 7, 0, 48);
+        }
+
+        private void lblButton_MouseLeave(object sender, EventArgs e)
+        {
+            lblButton.BackColor = Color.Transparent;
+        }
+    }
+}
